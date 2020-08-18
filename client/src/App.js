fff5e1db774a708fc2154961359934ff6caa311c
AdamWang00 <AdamWang00@users.noreply.github.com>
2020-08-18 03:17:09 -0400

    NUNU AND WILLUMP

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import './App.css';
 import Hello from './Hello.js';
-import { Sampler } from 'tone';
 
 const App = () => {
-  //create a synth and connect it to the main output (your speakers)
-  const synth = new Tone.Synth().toDestination();
-
-  //play a middle 'C' for the duration of an 8th note
-  synth.triggerAttackRelease("C4", "8n");
   const [h, setH] = React.useState(1);
 
   const lol = x => x * x;
@@ -23,8 +17,8 @@ const App = () => {
     <div className="App">
       {h}
       <br />
-      <Hello h={h} update={updateMe} squid="hello world" func={lol} />
-      <Hello h={h} update={updateMe} squid="squidward" func={lolol} />
+      <Hello note="C4" h={h} update={updateMe} squid="hello world" func={lol} />
+      <Hello note="E4" h={h} update={updateMe} squid="squidward" func={lolol} />
     </div>
   );
 }
--- client/src/Hello.js
+++ client/src/Hello.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import * as mm from "@magenta/music";
+import * as Tone from 'tone';
+import * as p5 from 'p5';
+import * as ml5 from 'ml5';
 
 export default (props) => {
+  const synth = new Tone.Synth().toDestination();
 
   const [foo, setFoo] = React.useState(1);
 
@@ -12,7 +17,10 @@ export default (props) => {
     <div>
       { props.squid + " " + props.func(props.h) }
       <br />
-      <button onClick={() => props.update()}>butt</button>
+      <button onClick={() => {
+        props.update();
+        synth.triggerAttackRelease(props.note, "8n");
+      }}>Play {props.note}</button>
     </div>
   );
   
